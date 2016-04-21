28b0883744e7bbc0cfbfae6c89771431858af5e1
SkaterDanny <dannykruiswijk@hotmail.com>
2016-04-21 11:07:50 +0200

    Added Basic Movement, Camera Follower

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehaviour : MonoBehaviour {
+
+    [SerializeField] private Transform target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(transform.position.x,target.transform.position.y+5f,transform.position.z);
+	}
+}
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+    private Vector3 mousePosition;
+    private float moveSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mousePosition = new Vector3(Input.mousePosition.x,Input.mousePosition.y,Input.mousePosition.z);
+        mousePosition.z = 5f;
+        //mousePosition.y = transform.position.y;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        Debug.Log(mousePosition);
+        transform.position = new Vector3(mousePosition.x,transform.position.y,mousePosition.z);
+        //transform.position = Vector3.Lerp(transform.position,mousePosition,moveSpeed*Time.deltaTime);
+    }
+}
