95bd81fcc31c441fdc7d094640b3bf1ddbf90d07
SkaterDanny <dannykruiswijk@hotmail.com>
2016-04-22 15:37:20 +0200

    Fading Objects

---
+++ Assets/Scripts/CameraObjectFader.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraObjectFader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit[] hit = Physics.SphereCastAll(transform.position,5f,-Vector3.up,3f, LayerMask.GetMask("Level"));
+        for(int i = 0; i < hit.Length; i++)
+        {
+            float Yoffset;
+            switch (hit[i].transform.name)
+            {
+                case "pCone":
+                    Yoffset = 1f;
+                    break;
+                default:
+                    Yoffset = 0f;
+                    break;
+
+            }
+            float distance =  transform.position.y - (hit[i].transform.position.y+Yoffset);
+            Renderer renderer = hit[i].transform.GetComponent<Renderer>();
+            renderer.material.color = new Color(renderer.material.color.r,renderer.material.color.g,renderer.material.color.b,(distance/10));
+        }
+    }
+}
---
+++ Assets/Scripts/TransparentTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransparentTest : MonoBehaviour {
+
+    private Renderer renderer;
+    [SerializeField] private Transform target;
+    private float distance;
+    private float transparentValue;
+    [SerializeField] private float yOffset;
+
+	// Use this for initialization
+	void Start () {
+        renderer = GetComponent<Renderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        distance = target.position.y - (transform.position.y+yOffset);
+        transparentValue = (distance / 5)-1f;
+        //Debug.Log(distance);
+        Debug.Log(transparentValue);
+        renderer.material.color = new Color(renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, transparentValue);
+    }
+}
