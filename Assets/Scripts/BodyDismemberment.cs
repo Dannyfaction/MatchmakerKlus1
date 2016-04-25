25f8a105373b2e4020d62a988ef86e3f384702db
Daankovski <danielepke@hotmail.com>
2016-04-25 23:04:07 +0200

    working on Body Dismemberment

---
+++ Assets/Scripts/BodyDismemberment.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BodyDismemberment : MonoBehaviour {
+    
+    void OnCollisionEnter(Collision col) {
+
+        if (col.gameObject.tag != gameObject.tag) {
+            if (gameObject.GetComponent<CharacterJoint>())
+            {
+                GameObject.Destroy(gameObject.GetComponent<CharacterJoint>());
+            }
+            if (gameObject.GetComponent<Collider>())
+            {
+                GameObject.Destroy(gameObject.GetComponent<Collider>());
+            }
+            if (gameObject.GetComponent<Rigidbody>())
+            {
+                GameObject.Destroy(gameObject.GetComponent<Rigidbody>());
+            }
+
+            GameObject.Destroy(gameObject);
+        } 
+    }
+}
